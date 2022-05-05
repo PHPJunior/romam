bc11ed0236a3f822a3f79bb42f9f1a0367146020
Nyi Nyi Lwin <>
2022-05-05 14:05:20 +0630

    update robot

--- romam.php
+++ romam.php
@@ -7,47 +7,48 @@ const MULTIPLIER_4 = -1;
 
 function finalPosition($move)
 {
-    $path = $move;
+    preg_match_all("/[A-Z]+|\d+/", $move, $path);
 
     // Current position of the robot
     $x = 0;
     $y = 0;
     $directionNumber = 1;
 
-    $direction = [ 
-        1 => 'TOP', 
-        2 => 'RIGHT', 
-        3 => 'BOTTOM', 
+    $direction = [
+        1 => 'TOP',
+        2 => 'RIGHT',
+        3 => 'BOTTOM',
         4 => 'LEFT'
     ];
-    
-    for($i = 0; $i < strlen($path); $i++)
-    {
-        switch ($path[$i]) {
 
-            case 'R':
-                if($directionNumber == 4){
-                    $directionNumber = 1;
-                } else {
-                    $directionNumber++;
-                }
-                break;
+    foreach ($path[0] as $key => $value) {
+        for($i = 0; $i < strlen($value); $i++)
+        {
+            switch ($value[$i]) {
+                case 'R':
+                    if($directionNumber == 4){
+                        $directionNumber = 1;
+                    } else {
+                        $directionNumber++;
+                    }
+                    break;
 
-            case 'L':
-                if($directionNumber == 1){
-                    $directionNumber = 4;
-                } else {
-                    $directionNumber--;
-                }
-                break;
-            
-            case 'F':
-                if( !($directionNumber % 2) ){
-                    $x += $path[$i+1] * constant("MULTIPLIER_".$directionNumber);
-                } else {
-                    $y += $path[$i+1] * constant("MULTIPLIER_".$directionNumber);
-                }
-                break;
+                case 'L':
+                    if($directionNumber == 1){
+                        $directionNumber = 4;
+                    } else {
+                        $directionNumber--;
+                    }
+                    break;
+
+                case 'F':
+                    if( !($directionNumber % 2) ){
+                        $x += $path[0][$key+1] * constant("MULTIPLIER_".$directionNumber);
+                    } else {
+                        $y += $path[0][$key+1] * constant("MULTIPLIER_".$directionNumber);
+                    }
+                    break;
+            }
         }
     }
 
